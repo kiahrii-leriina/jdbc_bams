ea206da83e0a612c2d30c71277b10c5178a3b522
kaihrii leriina <litetencent5@gmail.com>
2025-05-21 23:36:55 +0530

    main accountdao completed

--- src/main/java/bams/AccountDAO.java
+++ src/main/java/bams/AccountDAO.java
@@ -13,104 +13,115 @@ public class AccountDAO {
 		try {
 			Connection con = DBConnection.getConnection();
 			PreparedStatement ps = con.prepareStatement(sql);
-			
+
 			ps.setString(1, name);
 			ps.setDouble(2, balance);
-			
+
 			int rows = ps.executeUpdate();
-			if(rows>0) {
+			if (rows > 0) {
 				System.out.println("Account Created Successfully");
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	public void getAccountDetials(int accountId) {
 		String sql = "SELECT * FORM accounts WHERE account_id = ?";
-		
+
 		try {
 			Connection con = DBConnection.getConnection();
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps.setInt(1, accountId);
 			ResultSet rs = ps.executeQuery();
-			if(rs.next()) {
-				System.out.println("Account Id: "+rs.getInt("account_id"));
-				System.out.println("Name: "+rs.getString("name"));
-				System.out.println("Available Balance: ₹"+rs.getDouble("balance"));
+			if (rs.next()) {
+				System.out.println("Account Id: " + rs.getInt("account_id"));
+				System.out.println("Name: " + rs.getString("name"));
+				System.out.println("Available Balance: ₹" + rs.getDouble("balance"));
 			}
-			
+
 			else {
-				System.out.println("No account found with the given id: "+accountId);
+				System.out.println("No account found with the given id: " + accountId);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void deposit(int accountId, double amount) {
 		String sql = "UPDATE accounts SET balance = ? WHERE account_id = ?";
-		
+
 		try {
 			Connection con = DBConnection.getConnection();
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps.setDouble(1, amount);
 			ps.setInt(2, accountId);
-			
+
 			int rows = ps.executeUpdate();
-			if(rows>0) {
-				System.out.println("Deposit Successfull");
-			}
-			else {
+			if (rows > 0) {
+				try (PreparedStatement ps1 = con
+						.prepareStatement("SELECT balance FROM accounts WHERE account_id = ?")) {
+					
+					ps1.setInt(1, accountId);
+					ResultSet rs = ps1.executeQuery();
+					System.out.println("Deposit Successfull available balance: "+rs.getDouble("balance"));
+				}
+			} else {
 				System.out.println("Account not found");
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void withdraw(int accountId, double amount) {
 		String checkbalance = "SELECT balance FROM accounts WHERE account_id = ?";
 		String sql = "UPDATE accounts SET balance = balance - ? WHERE account_id = ?";
-		
+
 		try {
 			Connection con = DBConnection.getConnection();
-			
+
 			PreparedStatement ps1 = con.prepareStatement(checkbalance);
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps1.setInt(1, accountId);
 			ResultSet rs = ps1.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				double currentBalance = rs.getDouble("balance");
-				if(currentBalance >= amount) {
+				if (currentBalance >= amount) {
 					ps.setDouble(1, amount);
 					ps.setInt(2, accountId);
 					ps.executeUpdate();
-					System.out.println("Withdrawal successfull, withfrawal amount: ₹"+amount);
-				}
-				else {
+					System.out.println("Withdrawal successfull, withfrawal amount: ₹" + amount);
+				} else {
 					System.out.println("Insufficient balance.");
 				}
-			}
-			else {
+			} else {
 				System.out.println("Account not found");
 			}
-			
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	
-	
-	
-	
-	
-	
-	
+	public void checkAccountBalance(int accountId) {
+		String sql = "SELECT balance FROM accounts WHERE account_id = ?";
+		try(Connection con = DBConnection.getConnection()){
+			try(PreparedStatement ps = con.prepareStatement(sql)){
+				ps.setInt(1, accountId);
+				ResultSet rs = ps.executeQuery();
+				if(rs.next()) {
+					System.out.println("Available balance "+rs.getDouble("balance"));
+				}
+				System.out.println("Account not found");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
--- src/main/java/bams/Main.java
+++ src/main/java/bams/Main.java
@@ -1,5 +1,96 @@
 package bams;
 
+import java.util.Scanner;
+
 public class Main {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		AccountDAO accountdao = new AccountDAO();
+		TransactionDAO transactiondao = new TransactionDAO();
+		
+		System.out.println("🟢⭐Welcome to BAMS⭐🟢");
+		
+		while(true) {
+			System.out.println("\n==========MENU===============");
+			System.out.println("1. Create account");
+			System.out.println("2. See account detials");
+			System.out.println("3. Deposit");
+			System.out.println("4. Withdraw");
+			System.out.println("5. Check account balance");
+			System.out.println("6. Transfer");
+			System.out.println("7. View transaction history");
+			System.out.println("8. exit");
+			
+			switch(sc.nextInt()) {
+				case 1:{
+					sc.nextLine();
+					System.out.println("Enter account holder name");
+					String name = sc.nextLine();
+					System.out.println("Enter initial deposit amount");
+					double amount;
+					try {
+						amount = Double.parseDouble(sc.nextLine());
+					}catch(NumberFormatException e) {
+						System.out.println("Invalid amount");
+						break;
+					}
+					if(amount < 0 || name.isBlank()) {
+						System.out.println("Invalid input, Name cannot be blank and amount must not be negative");
+						break;
+					}
+					accountdao.createAccount(name, amount);
+					break;
+				}
+				
+				case 2:{
+					System.out.println("Enter Account Id to check account detials");
+					int id = sc.nextInt();
+					accountdao.getAccountDetials(id);
+					break;
+				}
+				
+				case 3:{
+					System.out.println("Enter Account ID:");
+					int id= sc.nextInt();
+					System.out.println("Enter amount:");
+					double amount = sc.nextDouble();
+					if(amount < 0) {
+						System.out.println("Invalid amount ");
+						break;
+					}
+					accountdao.deposit(id, amount);
+					break;
+				}
+				
+				case 4:{
+					System.out.println("Enter Account Id:");
+					int id = sc.nextInt();
+					System.out.println("Enter amount to withdraw");
+					double amount = sc.nextDouble();
+					if(amount<0) {
+						System.out.println("Invalid amount");
+						break;
+					}
+					accountdao.withdraw(id, amount);
+					break;
+				}
+				
+				case 5:{
+					System.out.println("Enter Acount Id:");
+					int id = sc.nextInt();
+					accountdao.checkAccountBalance(id);
+					break;
+				}
+				case 6:{
+					System.out.println("Enter account id to transfer from");
+					int fromId = sc.nextInt();
+					System.out.println("Enter account id to transfer to ");
+					int toId = sc.nextInt();
+					System.out.println("Enter amount to transer");
+					
+				}
+			}
+		}
+	}
 }
